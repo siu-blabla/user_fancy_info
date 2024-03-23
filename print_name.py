f8c9bec0d3cf68b2ba947fcb283e951c47d76ca1
siu-blabla <107726897+siu-blabla@users.noreply.github.com>
2024-03-23 23:24:59 +0800

    Added pseudocode

---
+++ print_name.py
@@ -0,0 +1,10 @@
+# pseudocode
+
+# ask user to input name
+# ask user to input their dream job
+# ask user what their hobby is
+# print name in fancy way
+# print dream job in fancy way
+# print hobby in fancy way
+# display the inputs of the user
+# End
