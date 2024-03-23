5dade2964bcc37d27fe97fa7261d963aca5f25ca
siu-blabla <107726897+siu-blabla@users.noreply.github.com>
2024-03-23 23:30:48 +0800

    Ask user what their hobby is

--- print_name.py
+++ print_name.py
@@ -5,6 +5,7 @@ user_name = str(input("What is your name? (Given name only): "))
 # ask user to input their dream job
 user_dream_job = str(input("What is your dream job?: "))
 # ask user what their hobby is
+user_hobby = str(input("What is your hobby?: "))
 # print name in fancy way
 # print dream job in fancy way
 # print hobby in fancy way
